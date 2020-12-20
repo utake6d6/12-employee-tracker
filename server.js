caa515d75d792f33df2fe3a3c9b177462d31148a
Bert <robertgill82@gmail.com>
2020-12-19 17:50:52 -0700

    initial pseudo code

---
+++ server.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+//
+
+//
+
+//
+
+// main menu
+
+// edit
+
+// menu choice
+
+// build departments
+
+// build roles
+
+// build employees
