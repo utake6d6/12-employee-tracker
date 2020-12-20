e3ba19dc87be77521786c8c70e952ce0fa58fdba
Bert <robertgill82@gmail.com>
2020-12-19 18:24:02 -0700

    connection pool - main and async function added - test

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-//
+// MySQL’s connection pool - reuse connections - enhance performance of executing commands
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  database: "test",
+});
 
-//
+// run
+main();
 
-//
+// async function for main menu
+async function main() {
+  await mainMenu();
+  process.exit();
+}
 
 // main menu
 
