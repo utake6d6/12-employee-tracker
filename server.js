0cbd50e6ab369e19392c7cc027d585458cef5b54
Bert <robertgill82@gmail.com>
2020-12-20 21:10:56 -0700

    build roles and employees - new value choices - promise

--- server.js
+++ server.js
@@ -54,17 +54,46 @@ async function mainMenu() {
   // build departments
   var departments = await query("select * from department order by id", []);
   var deptChoices = [];
-  // new value for choices
+  // new value for choice
   for (let d of departments) {
     deptChoices.push({ name: d.NAME, value: d });
   }
 
   // build roles
+  var roles = await query(
+    "select r.*, d.NAME as DEPARTMENT_NAME from role r join department d on (r.department_id = d.id) order by r.id",
+    []
+  );
+  var roleChoices = [];
+  // new value for choice
+  for (let r of roles) {
+    roleChoices.push({ name: r.TITLE, value: r });
+  }
 
   // build employees
+  var employees = await query("select * from employee", []); // come back for sql - see notes
+  var employeeChoices = [];
+  // new value for choice
+  for (let e of employees) {
+    employeeChoices.push({ name: e.FIRST_NAME + " " + e.LAST_NAME, value: e });
+  }
+  employeeChoices.push({name: '[None]', value: {ID: null}});
+
+  switch
 }
 
 // helper functions
 
 // tutor notes: promise needs 2 functions
-//
+// Build Employees to get the manager name need to join back on emp table - once this join happens you want to have a left join - 3 joins (1 needs to be a left join - join/left join/join) -
+// JOIN combines records from two tables, locates related column values in the two tables, a query can contain zero, one, or multiple JOIN operations and INNER JOIN is the same as JOIN; the keyword INNER is optional
+// > exp:
+// "SELECT
+// country.country_name_eng, city.city_name, customer.customer_name,
+// FROM country,
+// JOIN city
+// ON city.country_id = country.id,
+// LEFT JOIN customer
+// ON customer.city_id = city.id
+// JOIN
+// WHERE condition",
